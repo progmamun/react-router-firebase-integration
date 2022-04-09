2324831eaad1af28688023b7b925b749f9ca5d4e
progmamun <mamun333089@gmail.com>
2022-04-09 13:35:06 +0600

    google-sign-hook-state-change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
--- src/hooks/useFirebase.jsx
+++ src/hooks/useFirebase.jsx
@@ -1,9 +1,22 @@
 import { useState, useEffect } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import app from '../firebase.init';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 const useFirebase = () => {
   const [user, setUser] = useState({});
 
   useEffect(() => {}, []);
 
-  const signInWithGoogle = () => {};
-  return [user, setUser];
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider).then(result => {
+      const user = result.user;
+      setUser(user);
+    });
+  };
+  // return [user, setUser];
+  return { user, setUser, signInWithGoogle };
 };
+
+export default useFirebase;
