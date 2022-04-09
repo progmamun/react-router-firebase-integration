25aaccea1eb95c777c2556dfe20a5ee441cdcfdb
progmamun <mamun333089@gmail.com>
2022-04-09 21:05:45 +0600

    auth-redirect

--- src/components/RequireAuth/RequireAuth.jsx
+++ src/components/RequireAuth/RequireAuth.jsx
@@ -10,7 +10,7 @@ const RequireAuth = ({ children }) => {
   const [user] = useAuthState(auth);
   const location = useLocation();
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
   return children;
 };
