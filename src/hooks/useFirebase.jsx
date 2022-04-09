8c6ced7a0a9ac97372efac637a9b9afe7f88a93a
progmamun <mamun333089@gmail.com>
2022-04-09 15:09:41 +0600

    sign-out-display-user-logged

--- src/hooks/useFirebase.jsx
+++ src/hooks/useFirebase.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 import app from '../firebase.init';
 
 const auth = getAuth(app);
@@ -7,16 +13,23 @@ const googleProvider = new GoogleAuthProvider();
 const useFirebase = () => {
   const [user, setUser] = useState({});
 
-  useEffect(() => {}, []);
-
   const signInWithGoogle = () => {
     signInWithPopup(auth, googleProvider).then(result => {
       const user = result.user;
       setUser(user);
     });
   };
+  const handleSignOut = () => {
+    signOut(auth).then(() => {});
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      setUser(user);
+    });
+  }, []);
   // return [user, setUser];
-  return { user, setUser, signInWithGoogle };
+  return { user, setUser, handleSignOut, signInWithGoogle };
 };
 
 export default useFirebase;
